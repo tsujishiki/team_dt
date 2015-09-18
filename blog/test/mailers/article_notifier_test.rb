8ea7fc3bd58675e657377fe80a530b1675a13181
Tsuji Shiki <tsujishiki@gmail.com>
2015-09-18 15:40:43 +0800

    test类

--- blog/test/mailers/article_notifier_test.rb
+++ blog/test/mailers/article_notifier_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ArticleNotifierTest < ActionMailer::TestCase
   test "received" do
-    mail = ArticleNotifier.received
+    mail = ArticleNotifier.received(articles(:one))
     assert_equal "Received", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
